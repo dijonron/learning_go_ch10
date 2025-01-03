74b644d0cae588e171639b15b5697215fbe7c2d7
dijonron <dronan@ualberta.ca>
2025-01-03 11:44:09 -0700

    add module and function documentation

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
+// Package ch10_exercise implements a simple Add function for the solution to Chapter 10 of Learning Go, 2nd Edition.
 package ch10_exercise
 
+// Add is a function that takes two integers and returns their sum.
+// See [Add] for implementation details.
+//
+// [Add]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
